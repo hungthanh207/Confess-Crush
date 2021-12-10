f47d652abd28ee147239c82c0874dadd9a4d9607
hungthanh207 <95893281+hungthanh207@users.noreply.github.com>
2021-12-10 11:13:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,20 @@
 const CONFIG = {
-    titleWeb: "yêu Bé Đạt:3",
-    introTitle: 'Trương Phương Anh',
-    introDesc: `Ốm một mình, đau cũng một mình. ĐẠT không thương em thì ai thương em đây?`,
+    titleWeb: "Vợ iu",
+    introTitle: 'Yêu Nhi',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng Đạt đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng đẠT đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng Zinh đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng Zinh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Yêu lắm <33',
-    btnNo: 'Hông nha :3',
-    question: 'Tại sao Đạt cute lại thích mình vậy:3',
+    btnNo: 'Hông nha :(',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'tại vì bạn xin quá mà:3',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi hotel nha.',
+    reply: ':>Yêu bạn thì đâu cần lí do',
+    mess: 'Yêu vợ nhìu nhìu',
+    messDesc: 'Mong 2 ta gặp được nhau ở một ngày không xa:3',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
